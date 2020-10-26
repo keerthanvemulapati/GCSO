030e5204af35285fef354300b869c36792e074e4
keerthanvemulapati <68475308+keerthanvemulapati@users.noreply.github.com>
2020-10-26 19:19:34 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,8 +4,12 @@ function setup() {
   speed=random(55,90);
   wall=random(400,1500);
   car=createSprite(50,200,50,50);
-  wall=createSprite(1500,200,60,height/2)
+  car.shapeColor = "red";
+  wall=createSprite(1500,200,60,height/2);
+  wall.shapeColor = "orange";
   
+var deformation = 0.5*weight*speed*speed/22500;
+
   createCanvas(800,400);  
   createSprite(400, 200, 50, 50);
 }
@@ -13,7 +17,11 @@ function setup() {
 function draw() {
   background("white");
   car.velocityX=speed;
-  if(wall.x-car.x < (car.width+wall.width)/2)
+  if(car.isTouching(wall))
+  {
+    car.color="black";
+  }
+  if(car.isTouching(wall))
   {
     car.velocityX=0;
     var deformation=0.5 * width * speed * speed/22500;
@@ -30,6 +38,6 @@ function draw() {
       car.shapeColor=color("green");
     }
   }  
+
   drawSprites();
   
-}
